47e3841d64b6ad11b445da013cd592f3160e2ae8
Denis Khodakovskiy <denis.khodakovskiy@gmail.com>
2017-02-09 19:09:42 +0300

    Strategy

--- Strategy/index.php
+++ Strategy/index.php
@@ -10,5 +10,6 @@ spl_autoload_register(function ($className) {
     if (file_exists("{$className}.php")) require "{$className}.php";
 });
 
-$compressor = (isset($_GET['type']) && $_GET['type'] == 'zip') ? new ZipCompressor() : new RarCompressor();
-echo $compressor->pack('SomeFile');
+$strategy = (isset($_GET['type']) && $_GET['type'] == 'zip') ? new ZipCompressor() : new RarCompressor();
+$compressor = new Compressor($strategy);
+echo $compressor->compress('SomeFile');
